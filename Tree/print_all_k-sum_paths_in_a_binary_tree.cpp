082a43a1d831dd81ca55eeec74ad92713894a334
dexur-swaroop <jyothiswaroop@dexur.com>
2025-03-05 00:04:12 +0530

    Trees pring all k sum paths in a binary tree

--- Tree/print_all_k-sum_paths_in_a_binary_tree.cpp
+++ Tree/print_all_k-sum_paths_in_a_binary_tree.cpp
@@ -1,31 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-
 class Node {
 public:
     int data;
-    Node* left;
-    Node* right;
-
-    Node(int d) {
-        data = d;
+    Node* left, *right;
+    Node(int val) {
+        data = val;
         left = right = nullptr;
     }
 };
 
-void findKSumPaths(Node* root, vector<int>& path, int k) {
-    if(!root) return;
-    path.push_back(root->data);
-
+void printPath(vector<int>& path, int j) {
+    cout << "Path: ";
+    for(int i=j; i<path.size(); i++) {
+        cout << path[i] << " ";
+    }
+    cout << endl;
 }
 
+void findKSumPaths(Node* root, vector<int>& path, int K) {
+    if (!root) return;
+
+    // Add current node to path
+    path.push_back(root->data);
+
+    // Check for K-sum paths ending at this node
+    int sum = 0;
+    for (int i = path.size() - 1; i >= 0; i--) {
+        sum += path[i];
+        if (sum == K) {
+            printPath(path, i);
+        }
+    }
+
+    // Recur for left and right subtrees
+    findKSumPaths(root->left, path, K);
+    findKSumPaths(root->right, path, K);
+
+    // Remove the current node (Backtracking)
+    path.pop_back();
+}
+
+// Wrapper function
 void printKSumPaths(Node* root, int K) {
     vector<int> path;
     findKSumPaths(root, path, K);
 }
 
+// Driver Code
 int main() {
     Node* root = new Node(1);
     root->left = new Node(3);
@@ -42,5 +65,6 @@ int main() {
     int K = 5;
     cout << "Paths with sum " << K << " are:\n";
     printKSumPaths(root, K);
+    
     return 0;
 }
