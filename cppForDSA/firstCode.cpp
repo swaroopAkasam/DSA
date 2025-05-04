c90e3819797376db762970b2bbe3c4ddffd452cf
dexur-swaroop <jyothiswaroop@dexur.com>
2025-05-05 00:29:28 +0530

    [CPP/DSA]

---
+++ cppForDSA/firstCode.cpp
@@ -0,0 +1,32 @@
+// Byte Babu: C++ DSA Series
+// Video 1: Your First C++ Program
+
+// // Code Snippet
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     cout << "Hello CPP!" << endl;
+//     return 0;
+// }
+
+
+// Explanation
+
+#include <bits/stdc++.h>
+// includes all the libraries and modules etc needed in on go.
+
+using namespace std;
+// Without using namespace std;, you'd have to prefix everything from the standard library with std:: to access them. 
+// For example:
+// std::cout << "Hello, World!" << std::endl;
+// cout << "Hello, World!" << endl; // instead of the above line u can just go with this line.
+
+
+int main() { // Main is the starting function u can say.
+    cout << "Hello CPP!" << endl; // cout is to print anything to the terminal, and endl is to print new line.
+    return 0; // just returning zero
+}
+
+
+Thank you see yah in next Video.
