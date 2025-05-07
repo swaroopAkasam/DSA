6b9cff1ed5199a5872c7b97a62785ea08e92b49d
dexur-swaroop <jyothiswaroop@dexur.com>
2025-05-08 01:42:14 +0530

    [DP] climbing staris

--- DSA-Leetcode/DynamicProgramming/coinChangeProblem.cpp
+++ DSA-Leetcode/DynamicProgramming/coinChangeProblem.cpp
@@ -4,20 +4,24 @@ You are given an integer array coins representing coins of different denominatio
 
 Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
 
-You may assume that you have an infinite number of each kind of co
+You may assume that you have an infinite number of each kind of coins supply
+
+
 Example 1:
 
-Input: coins = [1,2,5], amount = 11
-Output: 3
-Explanation: 11 = 5 + 5 + 1
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
 Example 2:
 
-Input: coins = [2], amount = 3
-Output: -1
-Example 3:
-
-Input: coins = [1], amount = 0
-Output: 0
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
  
 */
 
@@ -28,58 +32,31 @@ using namespace std;
 
 class Solution {
 public:
-    int solveRecursive(vector<int>& coins, int amount) {
-        if(amount==0) return 0;
-        if(amount<0) return INT_MAX;
-        int ans = INT_MAX;
-        for(auto ele: coins) {
-            int curr = solveRecursive(coins, amount-ele);
-            if(curr != INT_MAX) {
-                ans = min(ans, curr+1);
-            }
-        }
-        return ans;
+    int climbStairsMem(int n, vector<int>& dp) {
+        if(n<=2) return n;
+        if(dp[n] != -1) return dp[n];
+        dp[n] = climbStairs(n-1) + climbStairs(n-2);
+        return dp[n];
     }
-    int solveMem(vector<int>& coins, int amount, vector<int>& dp) {
-        if(amount==0) return 0;
-        if(amount<0) return INT_MAX;
-        if(dp[amount]!=-1) return dp[amount];
-        int ans = INT_MAX;
-        for(auto ele: coins) {
-            int curr = solveMem(coins, amount-ele, dp);
-            if(curr != INT_MAX) {
-                ans = min(ans, curr+1);
-            }
-        }
-        dp[amount] = ans; 
-        return dp[amount];
-    }
-    int solveTab(vector<int>& coins, int amount) {
-        vector<int> dp(amount+1, INT_MAX);
+    int climbStairsTab(int n) {
+        if(n<=2) return n;
+        vector<int> dp(n+1, -1);
         dp[0] = 0;
-        for(int i=1; i<=amount; i++) {
-            for(auto ele: coins) {
-                if(i-ele >=0 && dp[i-ele] != INT_MAX) {
-                    dp[i] = min(dp[i], dp[i-ele] + 1);
-                }
-            }
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i=3; i<=n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
         }
-        if(dp[amount] == INT_MAX) return -1;
-        return dp[amount];
+        return dp[n];
     }
 };
 int main() {
     Solution sol;
-    vector<int> coins = {1,2,5};
-    int amount = 11;
-    int ans =  sol.solveRecursive(coins, amount);
-    ans = ans == INT_MAX ? -1 : ans;
+    itn n = 4;
+    vector<int> dp(n+1, -1);
+    int ans = sol.climbStairsMem(n, dp);
     cout << ans << endl;
-    vector<int> dp(amount+1, -1);
-    ans = sol.solveMem(coins, amount, dp);
-    ans = ans == INT_MAX ? -1 : ans;
+    ans = sol.climbStairsTab(n);
     cout << ans << endl;
-    ans = sol.solveTab(coins, amount);
-    ans = ans == INT_MAX ? -1 : ans;
-    cout << ans << endl;
+    return 0;
 }
