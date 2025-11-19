fd31dcbc2234918c949652acf9ac5e7ba2edb37b
Swaroop <akasamjyothiswaroop93@gmail.com>
2025-11-19 16:21:14 +0530

    [Arrays] longest subsequence brute force, sorting approach

--- DSA-Leetcode/Arrays/longestConsecutiveSequence.cpp
+++ DSA-Leetcode/Arrays/longestConsecutiveSequence.cpp
@@ -8,6 +8,68 @@ You must write an algorithm that runs in O(n) time.
 #include <bits/stdc++.h>
 using namespace std;
 
+// Brute-force Approach: Start 
+bool linearSearch(vector<int>&a, int num) {
+    int n = a.size(); //size of array
+    for (int i = 0; i < n; i++) {
+        if (a[i] == num)
+            return true;
+    }
+    return false;
+}
+int longestSuccessiveElements(vector<int>&a) {
+    int n = a.size(); //size of array
+    int longest = 1;
+    //pick a element and search for its
+    //consecutive numbers:
+    for (int i = 0; i < n; i++) {
+        int x = a[i];
+        int cnt = 1;
+        //search for consecutive numbers
+        //using linear search:
+        while (linearSearch(a, x + 1) == true) {
+            x += 1;
+            cnt += 1;
+        }
+
+        longest = max(longest, cnt);
+    }
+    return longest;
+}
+// Brute-force Approach: End 
+
+
+// Better Approach(Using sorting): Start
+int longestSuccessiveElements(vector<int>&a) {
+    int n = a.size();
+    if (n == 0) return 0;
+
+    //sort the array:
+    sort(a.begin(), a.end());
+    int lastSmaller = INT_MIN;
+    int cnt = 0;
+    int longest = 1;
+
+    //find longest sequence:
+    for (int i = 0; i < n; i++) {
+        if (a[i] - 1 == lastSmaller) {
+            //a[i] is the next element of the
+            //current sequence.
+            cnt += 1;
+            lastSmaller = a[i];
+        }
+        else if (a[i] != lastSmaller) {
+            cnt = 1;
+            lastSmaller = a[i];
+        }
+        longest = max(longest, cnt);
+    }
+    return longest;
+
+}
+// Better Approach(Using sorting): End
+
+// Optimal Approach(Using Set data structure): Start
 int longestSuccessiveElements(vector<int>&a) {
     int n = a.size();
     if (n == 0) return 0;
@@ -34,10 +96,8 @@ int longestSuccessiveElements(vector<int>&a) {
         }
     }
     return longest;
-
 }
-
-
+// Optimal Approach(Using Set data structure): End
 
 int main()
 {
