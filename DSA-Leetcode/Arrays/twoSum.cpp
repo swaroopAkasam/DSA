069b9f4ca19732b64dbb49cbc59519060891e81d
Swaroop <akasamjyothiswaroop93@gmail.com>
2026-02-04 19:43:02 +0530

    [Arrays] two sum problem with sorting approach

--- DSA-Leetcode/Arrays/twoSum.cpp
+++ DSA-Leetcode/Arrays/twoSum.cpp
@@ -25,13 +25,40 @@ public:
         }
         return ans;
     }
+
+    vector<int> twoSumSorting(vector<int>& nums, int target) {
+        vector<int> ans;
+        int n = nums.size();
+        vector<pair<int, int>> numsWithIndex;
+        for(int i=0; i<n; i++) {
+            numsWithIndex.push_back({nums[i], i});
+        }
+
+        sort(numsWithIndex.begin(), numsWithIndex.end());
+
+        int left = 0, right = n-1;
+        while(left<right) {
+            int sum = numsWithIndex[left].first + numsWithIndex[right].first;
+            if(sum == target) {
+                ans.push_back(numsWithIndex[left].second); ans.push_back(numsWithIndex[right].second);
+                return ans;
+            }
+            else if(sum < target) {
+                left++;
+            }
+            else {
+                right--;
+            }
+        }
+        return ans;
+    }
 };
 
 int main() {
     Solution sol;
     vector<int>nums = {2,7,11,15};
     int target = 9;
-    vector<int> ans = sol.twoSum(nums, target);
+    vector<int> ans = sol.twoSumSorting(nums, target);
     for(auto ele: ans){
         cout << ele << " ";
     }
